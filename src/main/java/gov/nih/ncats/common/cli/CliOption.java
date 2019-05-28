8f9e8bba0e90dabb03c1bdba7246b585dcc54c49
katzelda <daniel.katzel@nih.gov>
2019-05-28 19:52:13 -0400

    moved cli package it its own project

--- src/main/java/gov/nih/ncats/common/cli/CliOption.java
+++
@@ -1,9 +0,0 @@
-package gov.nih.ncats.common.cli;
-
-/**
- * Created by katzelda on 6/21/17.
- */
-public interface CliOption {
-
-    void visit( OptionVisitor visitor);
-}
--- src/main/java/gov/nih/ncats/common/cli/OptionGroup.java
+++
@@ -1,7 +0,0 @@
-package gov.nih.ncats.common.cli;
-
-/**
- * Created by katzelda on 6/21/17.
- */
-public interface OptionGroup extends CliOption {
-}
