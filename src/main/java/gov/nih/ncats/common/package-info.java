83c7dc893a0214812a5a3fdae52e6652dd1b6235
katzelda <daniel.katzel@nih.gov>
2019-06-10 10:54:49 -0400

    added copyright info

---
+++ src/main/java/gov/nih/ncats/common/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * NCATS-COMMON contains utility classes that
+ * are used by several NCATS projects
+ * so it made sense to factor them out
+ * into their own project for easier reuse.
+ */
+package gov.nih.ncats.common;
